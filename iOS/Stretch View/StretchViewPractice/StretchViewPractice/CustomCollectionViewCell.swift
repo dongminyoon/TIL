7a90532d37b745f92d64b508d1f9b0bcf9781aff
dongminyoon <ehdals8965@naver.com>
2020-03-21 14:25:42 +0900

    StretchView 예제파일 추가

---
+++ iOS/Stretch View/StretchViewPractice/StretchViewPractice/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  StickyViewPractice
+//
+//  Created by 윤동민 on 2020/03/20.
+//  Copyright © 2020 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    static let identifier = "customCell"
+}
